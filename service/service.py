d36264cf7df8bc52b3102512f2f7d72abbb215ad
Manuel Cueto <tazc54@gmail.com>
2019-11-25 22:03:19 -0600

    version 2.2

--- service/service.py
+++ service/service.py
@@ -40,3 +40,63 @@ notification_fields = {
 }
 
 notification_manager = NotificationManager()
+
+class Notification(Resource):
+    def abort_if_notification_not_found(self, id):
+        if id not in notification_manager.notifications:
+            abort(
+                HttpStatus.not_found_404.value,
+                message = "Notification {0} doesn't exist".format(id))
+
+    @marshal_with(notification_fields)
+    def get(self, id):
+        self.abort_if_notification_not_found(id)
+        return notification_manager.get_notification(id)
+
+    def delete(self, id):
+        self.abort_if_notification_not_found(id)
+        notification_manager.delete_notification(id)
+        return '', HttpStatus.no_content_204.value
+
+    @marshal_with(notification_fields)
+    def patch(self, id):
+        self.abort_if_notification_not_found(id)
+        notification = notification_manager.get_notification(id)
+        parser = reqparse.RequestParser()
+        parser.add_argument('message', type = str)
+        parser.add_argument('ttl', type = int)
+        parser.add_argument('displayed_times', type = int)
+        parser.add_argument('displayed_once', type = bool)
+        args = parser.parse_args()
+        print(args)
+        if 'message' in args and args['message'] is not None:
+            notification.message = args['message']
+        if 'ttl' in args and args['ttl'] is not None:
+            notification.ttl = args['ttl']
+        if 'displayed_times' in args and args['displayed_times'] is not None:
+            notification.displayed_times = args['displayed_times']
+        if 'displayed_once' in args and args['displayed_once'] is not None:
+            notification.displayed_once = args['displayed_once']
+
+        return notification
+
+class NotificationList(Resource):
+    @marshal_with(notification_fields)
+    def get(self):
+        return [v for v in notification_manager.notifications.values()]
+
+    @marshal_with(notification_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('message', type=str, required=True, help='Message cannot be blank!')
+        parser.add_argument('ttl', type=int, required=True, help='Time to live cannot be blank!')
+        parser.add_argument('notification_category', type=str, required=True, help='Notification category cannot be blank!')
+        args = parser.parse_args()
+        notification = NotificationModel(
+            message=args['message'],
+            ttl=args['ttl'],
+            creation_date=datetime.now(utc),
+            notification_category=args['notification_category']
+            )
+        notification_manager.insert_notification(notification)
+        return notification, HttpStatus.created_201.value
